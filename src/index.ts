a542659b550149ab0bc63117aa27d604ccdceb1a
Hattan Shobokshi <hattan@gmail.com>
2020-05-29 01:57:48 -0700

    added docker + inputs

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,39 @@
 import tl = require('azure-pipelines-task-lib/task');
 import { ToolRunner, IExecSyncOptions, IExecSyncResult, IExecOptions } from "azure-pipelines-task-lib/toolrunner";
-
+import path = require('path');
 
 async function run() {
     try {
-        const inputString: string | undefined = tl.getInput('samplestring', true);
-        if (inputString == 'bad') {
+        const server: string | undefined = tl.getInput('server', true);
+        const filePath: string | undefined = tl.getInput('files', true);
+        const fileName = path.basename(filePath);
+        const folder = path.dirname(filePath);
+
+        console.log('server', server);
+        console.log('fileName', fileName);
+        console.log('folder', folder);    
+
+        if (server == 'bad') {
             tl.setResult(tl.TaskResult.Failed, 'Bad input was given');
             return;
         }
 
+
         const docker = tl.tool(tl.which("docker"))
                .arg("run")
-               .arg("retaildevcrew/webvalidate")
-               .arg("--host")
-               .arg("https://www.microsoft.com")
+               .arg("-v")
+               .arg(`${folder}:/app/TestFiles`)
+               .arg("retaildevcrew/webvalidate:beta")
+               .arg("--server")
+               .arg(server)
                .arg("--files")
-               .arg("msft.json")
-               .arg("--version");               
-
+               .arg(fileName)        
    
         await docker.exec();
-        console.log('Hello', inputString);
+
     }
     catch (err) {
-        //tl.setResult(tl.TaskResult.Failed, err.message);
+       // tl.setResult(tl.TaskResult.Failed, err.message);
     }
 }
 
